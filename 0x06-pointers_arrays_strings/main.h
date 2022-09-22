5fff844046c06bc9281dfa273c94927ff9f76851
Nikky-hub <106401984+Nikky-hub@users.noreply.github.com>
2022-09-22 17:11:00 +0100

    102-magic.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
-#ifndef main.h
-#define main.c
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -11,4 +12,5 @@ char *cap_string(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-#endif
+
+#endif /* MAIN_H */
