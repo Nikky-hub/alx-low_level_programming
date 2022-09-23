af4afb9f9ae8ca096e9d849884b4b378f6ca2dde
Nikky-hub <106401984+Nikky-hub@users.noreply.github.com>
2022-09-23 12:28:17 +0100

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,89 +1,43 @@
-#include "sort.h"
-#include <stdlib.h>
+#include "main.h"
 #include <stdio.h>
 
 /**
- * TDMerge - sorts and merges the sub arrays in source
- * @start: starting index (inclusive) for the left sub array
- * @middle: end index (exclusive) for the left sub array and
- * starting index (inclusive) for the right sub array
- * @end: end index (exclusive) for the right sub array
- * @dest: destination for data
- * @source: source of data
- *
- * Return: void
+ * print_buffer - prints a buffer
+ * @b: buffer.
+ * @size: size of buffer.
+ * Return: no return.
  */
-void TDMerge(size_t start, size_t middle, size_t end, int *dest, int *source)
+void print_buffer(char *b, int size)
 {
-	size_t i, j, k;
+	int j, k, l;
 
-	printf("Merging...\n");
-	printf("[left]: ");
-	print_array(source + start, middle - start);
-	printf("[right]: ");
-	print_array(source + middle, end - middle);
-	i = start;
-	j = middle;
-	for (k = start; k < end; k++)
+	if (size <= 0)
+		printf("\n");
+	else
 	{
-		if (i < middle && (j >= end || source[i] <= source[j]))
+		for (j = 0; j < size; j += 10)
 		{
-			dest[k] = source[i];
-			i++;
-		}
-		else
-		{
-			dest[k] = source[j];
-			j++;
+			printf("%.8x:", j);
+			for (k = j; k < j + 10; k++)
+			{
+				if (k % 2 == 0)
+					printf(" ");
+				if (k < size)
+					printf("%.2x", *(b + k));
+				else
+					printf("  ");
+			}
+			printf(" ");
+			for (l = j; l < j + 10; l++)
+			{
+				if (l >= size)
+					break;
+				if (*(b + l) < 32 || *(b + l) > 126)
+					printf("%c", '.');
+				else
+					printf("%c", *(b + l));
+			}
+			printf("\n");
 		}
 	}
-	printf("[Done]: ");
-	print_array(dest + start, end - start);
-}
-
-/**
- * TDSplitMerge - recursively splits the array and merges the sorted arrays
- * @start: starting index (inclusive)
- * @end: end index (exclusive)
- * @array: the array to sort
- * @copy: a copy of the array
- *
- * Return: void
- */
-void TDSplitMerge(size_t start, size_t end, int *array, int *copy)
-{
-	size_t middle;
-
-	if (end - start < 2)
-		return;
-	middle = (start + end) / 2;
-	TDSplitMerge(start, middle, array, copy);
-	TDSplitMerge(middle, end, array, copy);
-	TDMerge(start, middle, end, array, copy);
-	for (middle = start; middle < end; middle++)
-		copy[middle] = array[middle];
-}
-
-/**
- * merge_sort - sorts an array of integers in ascending order using the
- * Merge sort algorithm
- * @array: array to sort
- * @size: size of the array
- *
- * Return: void
- */
-void merge_sort(int *array, size_t size)
-{
-	size_t i;
-	int *copy;
-
-	if (array == NULL || size < 2)
-		return;
-	copy = malloc(sizeof(int) * size);
-	if (copy == NULL)
-		return;
-	for (i = 0; i < size; i++)
-		copy[i] = array[i];
-	TDSplitMerge(0, size, array, copy);
-	free(copy);
 }
