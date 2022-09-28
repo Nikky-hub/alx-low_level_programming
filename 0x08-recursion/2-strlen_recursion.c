eb56789537c270a97620ed3586405563a2bb2179
Olanike Odunfa <odunfaolanike5@gmail.com>
2022-09-28 10:15:18 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * _strlen_recursion - Returns Length of String
+ * @s: string
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _strlen_recursion(char *s)
 {
 	if (*s != '\0')
